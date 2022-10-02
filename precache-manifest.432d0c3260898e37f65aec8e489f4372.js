320f82223cb2063dbf7be86519f35b4199daa291
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-10-02 16:58:18 -0400

    Updates

---
+++ precache-manifest.432d0c3260898e37f65aec8e489f4372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3006b3a9f82506518a8858da293bdede",
+    "url": "/SideBar/index.html"
+  },
+  {
+    "revision": "4f0d24c97bfcba0b7637",
+    "url": "/SideBar/static/css/main.e0f79945.chunk.css"
+  },
+  {
+    "revision": "00dabe24ad094e2a245a",
+    "url": "/SideBar/static/js/2.43f1e553.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SideBar/static/js/2.43f1e553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0d24c97bfcba0b7637",
+    "url": "/SideBar/static/js/main.88c7df0a.chunk.js"
+  },
+  {
+    "revision": "d69f6818cd1bea230fc1",
+    "url": "/SideBar/static/js/runtime-main.a2531aa0.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/SideBar/static/media/logo.2bb7da65.svg"
+  }
+]);
